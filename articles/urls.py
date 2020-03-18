7d68b45f0dbb2a5c0ec6fbff6b76e6bd13571143
TwilighterS <ilya475@rambler.ru>
2020-03-18 20:37:37 +0300

    Create Article App

--- articles/urls.py
+++ articles/urls.py
@@ -13,14 +13,10 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path, include 
+from django.urls import path
 from django.urls import re_path
 from . import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    re_path(r'^articles/$', include('articles.urls')),
-    re_path(r'^about/$', views.about),
-    re_path(r'^$', views.homepage),
+    re_path(r'^$', views.article_list),
 ]
--- twiublog/settings.py
+++ twiublog/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'articles',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'twiublog.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['twiublog/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [

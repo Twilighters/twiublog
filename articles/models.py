d3f7253dd89bdecd43efb38ef7842be0c7f3091d
TwilighterS <ilya475@rambler.ru>
2020-03-21 00:18:07 +0300

    preFinal Blog

--- articles/models.py
+++ articles/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Article (models.Model):
@@ -6,8 +7,11 @@ class Article (models.Model):
     slug = models.SlugField()
     body = models.TextField()
     date = models.DateTimeField(auto_now_add=True)
-    #add in thumbnail later
-    #add author later
+    thumb = models.ImageField(default='default.png', blank=True)
+    author = models.ForeignKey(User, default=None, on_delete=models.DO_NOTHING)
 
     def __str__(self):
         return self.title
+
+    def snippet(self):
+        return self.body[:50] + '...'
--- articles/urls.py
+++ articles/urls.py
@@ -17,6 +17,11 @@ from django.urls import path
 from django.urls import re_path
 from . import views
 
+app_name = 'articles'
+
 urlpatterns = [
-    re_path(r'^$', views.article_list),
+    re_path(r'^$', views.article_list, name="list"),
+    re_path(r'^create/$', views.article_create, name="create"),
+    re_path(r'^(?P<slug>[\w-]+)/$',views.article_detail, name="detail")
+    
 ]
